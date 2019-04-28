02a3ed59a1c554a2db80b8737bc520bf7f82aee3
ihsaro <idjazhossanee@gmail.com>
2019-04-28 20:48:51 +0400

    BACKEND CONSTRUCTION NEARLY DONE

--- js/index.js
+++ js/index.js
@@ -1,20 +1,33 @@
 var randomWord;
+var randomWordToken;
 
 function initGame() {
     var word = document.getElementById("lblWord");
     
     setRandomWord();
 
-    for(var i = 0; i < randomWord.length; i++) {
-        word.innerHTML += "_ ";
-    }
-
-    alert(randomWord);
-
+    word.innerHTML = "WORD: " + randomWordToken;
 }
 
 function validateUserInput() {
-
+    var userInput = document.getElementById("txtUserInput");
+    var temp = "";
+    if(randomWord.includes(userInput.value)) {
+        for(var i = 0; i < randomWord.length; i++) {
+            if(randomWord.charAt(i) == userInput.value) {
+                temp += userInput.value;
+            }
+            else if(randomWordToken[i] == "_") {
+                temp += "_";
+            }
+            else {
+                temp += randomWordToken[i];
+            }
+        }
+        alert(temp);
+        randomWordToken = temp;
+        document.getElementById("lblWord").innerHTML = "WORD: " + randomWordToken;
+    }
 }
 
 function setRandomWord() {
@@ -22,4 +35,11 @@ function setRandomWord() {
     var randomWordIndex = Math.floor(Math.random() * (+(words.length - 1) - +0)) + +0;
 
     randomWord = words[randomWordIndex];
+    randomWordToken = "";
+
+    for(var i = 0; i < randomWord.length; i++) {
+        randomWordToken += "_";
+    }
+
+    alert(randomWord);
 }
